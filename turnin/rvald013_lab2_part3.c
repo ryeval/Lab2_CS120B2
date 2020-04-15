945ef498a79aad324230aa61fdc87f0141fc877e
Ryan Valdeavil Valdeavilla <atomicfailure32@gmail.com>
2020-04-15 10:22:59 -0700

    finished 1-3 but no make test

---
+++ turnin/rvald013_lab2_part3.c
@@ -0,0 +1,38 @@
+/*	Author: Ryan Valdeavilla 
+ *  Partner(s) Name: 
+ *	Lab Section:026
+ *	Assignment: Lab #2  Exercise #3
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+	DDRA = 0x00;	DDRC = 0xFF;
+	PORTA = 0xFF;	PORTC = 0x00;
+	unsigned char cntavail;
+	unsigned char tmpA1;
+    /* Insert your solution below */
+    while (1) {
+	cntavail = 0;
+	tmpA1 = PINA & 0x0F;
+	for(unsigned i = 0; i < 3; ++i){
+		if((tmpA1 >> i) & 0x01){
+			cntavail = cntavail+1;
+		}
+	}
+	if(cntavail == 0){
+		cntavail = 0x80;
+	}
+	PORTC = cntavail;
+		
+    }
+    return 1;
+}
